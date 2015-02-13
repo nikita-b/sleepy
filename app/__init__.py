c0b7ebfb774423fbb2ab92bfe169d87ba9dcc3d8
Nikita Belecky <nikita@belecky.ru>
2015-02-14 01:28:42 +0300

    Off log

--- app/__init__.py
+++ app/__init__.py
@@ -38,16 +38,16 @@ lm.login_view = 'login'
 
 toolbar = DebugToolbarExtension(app)
 # LOG DEBUG
-if not app.debug:
-    import logging
-    from logging.handlers import RotatingFileHandler
-    file_handler = RotatingFileHandler('/home/apps/sheephappens.ru/sleep.log', 'a', 1 * 1024 * 1024, 10)
-    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s \
-                                                 [in %(pathname)s:%(lineno)d]'))
-    app.logger.setLevel(logging.INFO)
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
-    app.logger.info('microblog statup')
+#if not app.debug:
+#    import logging
+#    from logging.handlers import RotatingFileHandler
+#    file_handler = RotatingFileHandler('/home/apps/sheephappens.ru/sleep.log', 'a', 1 * 1024 * 1024, 10)
+#    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s \
+#                                                 [in %(pathname)s:%(lineno)d]'))
+#    app.logger.setLevel(logging.INFO)
+#    file_handler.setLevel(logging.INFO)
+#    app.logger.addHandler(file_handler)
+#    app.logger.info('microblog statup')
 
 
 
